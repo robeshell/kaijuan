c8da137b40d244b4f8e482ca9daaf265ff4eac1c
wangwenyu <wangwenyu9394@gmail.com>
2026-07-28 13:08:00 +0800

    chore(design): upgrade Kai Design lock to 0.4.4
    Sync brand tokens snapshot and lock file; update version test.

--- test/brand_tokens_generated_test.dart
+++ test/brand_tokens_generated_test.dart
@@ -4,7 +4,7 @@ import 'package:kaijuan/core/theme/brand_tokens.g.dart';
 
 void main() {
   test('runtime theme consumes generated brand tokens', () {
-    expect(kaiBrandSpecVersion, '0.4.0');
+    expect(kaiBrandSpecVersion, '0.4.4');
     expect(AppSpacing.x4, KaiBrandSpacing.x4);
     expect(AppRadii.card, KaiBrandRadii.card);
     expect(AppProductRadii.cover, KaiProductTokens.coverRadius);
